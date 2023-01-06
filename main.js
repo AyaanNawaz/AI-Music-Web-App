f7dafb205f31b74da8dd33cfd658967b3d639fc7
AyaanNawaz <98031436+AyaanNawaz@users.noreply.github.com>
2023-01-06 13:26:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-function preload(){
+song=""
 
+function preload(){
+song1 = loadSound("music1.mp3");
+song2 = loadSound("music2.mp3");
 }
 
 function draw(){
-image(video, 0, 0, 700, 545)
+image(video, 0, 0, 600, 500)
 }
 
 function setup(){
-canvas = createCanvas(700, 545);
+canvas = createCanvas(600, 500);
 video= createCapture(VIDEO);
 video.hide();
 canvas.center();
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on("pose", gotPoses);
 }
+
+function modelLoaded(){
+console.log("Model Is Loaded");
+}
+
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+}
+
+}
+
